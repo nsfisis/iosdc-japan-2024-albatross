7bd55ee264f7eefda6c1f71865a2c6287d7e20fa
nsfisis <nsfisis@gmail.com>
2024-07-28 19:41:04 +0900

    refactor(backend): move config-related code to config.go

---
+++ backend/config.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	dbHost     string
+	dbPort     string
+	dbUser     string
+	dbPassword string
+	dbName     string
+}
+
+func NewConfigFromEnv() (*Config, error) {
+	dbHost, exists := os.LookupEnv("ALBATROSS_DB_HOST")
+	if !exists {
+		return nil, fmt.Errorf("ALBATROSS_DB_HOST not set")
+	}
+	dbPort, exists := os.LookupEnv("ALBATROSS_DB_PORT")
+	if !exists {
+		return nil, fmt.Errorf("ALBATROSS_DB_PORT not set")
+	}
+	dbUser, exists := os.LookupEnv("ALBATROSS_DB_USER")
+	if !exists {
+		return nil, fmt.Errorf("ALBATROSS_DB_USER not set")
+	}
+	dbPassword, exists := os.LookupEnv("ALBATROSS_DB_PASSWORD")
+	if !exists {
+		return nil, fmt.Errorf("ALBATROSS_DB_PASSWORD not set")
+	}
+	dbName, exists := os.LookupEnv("ALBATROSS_DB_NAME")
+	if !exists {
+		return nil, fmt.Errorf("ALBATROSS_DB_NAME not set")
+	}
+	return &Config{
+		dbHost:     dbHost,
+		dbPort:     dbPort,
+		dbUser:     dbUser,
+		dbPassword: dbPassword,
+		dbName:     dbName,
+	}, nil
+}
--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/jackc/pgx/v5"
@@ -16,46 +15,6 @@ import (
 	"github.com/nsfisis/iosdc-2024-albatross/backend/api"
 	"github.com/nsfisis/iosdc-2024-albatross/backend/db"
 )
-
-type Config struct {
-	dbHost     string
-	dbPort     string
-	dbUser     string
-	dbPassword string
-	dbName     string
-}
-
-var config *Config
-
-func loadEnv() (*Config, error) {
-	dbHost, exists := os.LookupEnv("ALBATROSS_DB_HOST")
-	if !exists {
-		return nil, fmt.Errorf("ALBATROSS_DB_HOST not set")
-	}
-	dbPort, exists := os.LookupEnv("ALBATROSS_DB_PORT")
-	if !exists {
-		return nil, fmt.Errorf("ALBATROSS_DB_PORT not set")
-	}
-	dbUser, exists := os.LookupEnv("ALBATROSS_DB_USER")
-	if !exists {
-		return nil, fmt.Errorf("ALBATROSS_DB_USER not set")
-	}
-	dbPassword, exists := os.LookupEnv("ALBATROSS_DB_PASSWORD")
-	if !exists {
-		return nil, fmt.Errorf("ALBATROSS_DB_PASSWORD not set")
-	}
-	dbName, exists := os.LookupEnv("ALBATROSS_DB_NAME")
-	if !exists {
-		return nil, fmt.Errorf("ALBATROSS_DB_NAME not set")
-	}
-	return &Config{
-		dbHost:     dbHost,
-		dbPort:     dbPort,
-		dbUser:     dbUser,
-		dbPassword: dbPassword,
-		dbName:     dbName,
-	}, nil
-}
 
 const (
 	gameTypeGolf = "golf"
@@ -121,16 +80,14 @@ func handleGolfPost(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var err error
-	config, err = loadEnv()
+	config, err := NewConfigFromEnv()
 	if err != nil {
-		fmt.Printf("Error loading env %v", err)
-		return
+		log.Fatalf("Error loading env %v", err)
 	}
 
 	openApiSpec, err := api.GetSwaggerWithPrefix("/api")
 	if err != nil {
-		fmt.Printf("Error loading OpenAPI spec\n: %s", err)
-		return
+		log.Fatalf("Error loading OpenAPI spec\n: %s", err)
 	}
 
 	ctx := context.Background()
