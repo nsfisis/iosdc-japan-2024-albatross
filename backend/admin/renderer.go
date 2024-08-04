264fa15fb2ba5f0b9636cda44b64deb3c56aa99d
nsfisis <nsfisis@gmail.com>
2024-08-04 14:50:30 +0900

    feat(backend): serve /admin/* pages from api-server

---
+++ backend/admin/renderer.go
@@ -0,0 +1,49 @@
+package admin
+
+import (
+	"embed"
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var (
+	//go:embed templates
+	templatesFS embed.FS
+	//go:embed assets
+	assetsFS embed.FS
+)
+
+type Renderer struct {
+	templates map[string]*template.Template
+}
+
+func NewRenderer() *Renderer {
+	return &Renderer{
+		templates: make(map[string]*template.Template),
+	}
+}
+
+func (r *Renderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	tmpl, ok := r.templates[name]
+	if !ok {
+		t, err := template.ParseFS(templatesFS, "templates/base.html", "templates/"+name+".html")
+		if err != nil {
+			return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+		r.templates[name] = t
+		tmpl = t
+	}
+	return tmpl.ExecuteTemplate(w, name+".html", data)
+}
+
+func newAssetsMiddleware() echo.MiddlewareFunc {
+	return middleware.StaticWithConfig(middleware.StaticConfig{
+		Root:       "/assets",
+		Filesystem: http.FS(assetsFS),
+		IgnoreBase: true,
+	})
+}
