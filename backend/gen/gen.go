d936d587df9d96f373cf8ec35f0c6c6882083f32
nsfisis <nsfisis@gmail.com>
2024-08-08 01:07:15 +0900

    feat(backend): generate taskqueue/processor_wrapper.go by go generate

--- backend/gen/gen.go
+++ backend/gen/gen.go
@@ -3,3 +3,4 @@ package gen
 //go:generate go run github.com/sqlc-dev/sqlc/cmd/sqlc generate
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config oapi-codegen.yaml ../../openapi.yaml
 //go:generate go run ./api/handler_wrapper_gen.go -i ../api/generated.go -o ../api/handler_wrapper.go
+//go:generate go run ./taskqueue/processor_wrapper_gen.go -i ../taskqueue/tasks.go -o ../taskqueue/processor_wrapper.go
