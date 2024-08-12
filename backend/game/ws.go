1cd22671a6bacaa7bde2cd17f78a5ba8899e8d40
nsfisis <nsfisis@gmail.com>
2024-08-12 10:46:54 +0900

    feat(backend): increase max WebSocket message size from 512 byte to 50 KiB

--- backend/game/ws.go
+++ backend/game/ws.go
@@ -12,7 +12,7 @@ const (
 	writeWait      = 10 * time.Second
 	pongWait       = 60 * time.Second
 	pingPeriod     = (pongWait * 9) / 10
-	maxMessageSize = 512
+	maxMessageSize = 50 * 1024
 )
 
 var upgrader = websocket.Upgrader{
