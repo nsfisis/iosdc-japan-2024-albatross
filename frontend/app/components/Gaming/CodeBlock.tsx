0ad3546aa69d2269e5d739e9b62bc956e95e7f6e
nsfisis <nsfisis@gmail.com>
2024-08-18 14:09:11 +0900

    feat(frontend): wrap code block

--- frontend/app/components/Gaming/CodeBlock.tsx
+++ frontend/app/components/Gaming/CodeBlock.tsx
@@ -13,7 +13,7 @@ export default function CodeBlock({ code, language }: Props) {
 	const highlighted = highlight(code, languages[language]!, language);
 
 	return (
-		<pre className="bg-white resize-none h-full w-full rounded-lg border border-gray-300 p-2">
+		<pre className="h-full w-full p-2 bg-white rounded-lg border border-gray-300 whitespace-pre-wrap break-words resize-none">
 			<code dangerouslySetInnerHTML={{ __html: highlighted }} />
 		</pre>
 	);
