e20a0196cdd4a19636a12bcc392d79a803fd868e
nsfisis <nsfisis@gmail.com>
2024-08-18 13:32:18 +0900

    feat(frontend): improve styling of starting screen

--- frontend/app/components/GolfWatchApps/GolfWatchAppStarting.tsx
+++ frontend/app/components/GolfWatchApps/GolfWatchAppStarting.tsx
@@ -7,10 +7,8 @@ export default function GolfWatchAppStarting({
 }: Props) {
 	return (
 		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
-			<div className="text-center">
-				<h1 className="text-4xl font-bold text-black-600 mb-4">
-					Starting... ({timeLeft} s)
-				</h1>
+			<div className="text-center text-black font-black text-10xl animate-ping">
+				{timeLeft}
 			</div>
 		</div>
 	);
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
 	content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				"10xl": "16rem",
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config;
